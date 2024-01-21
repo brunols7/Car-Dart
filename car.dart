06181ede1184ec5fc3c16fbfb9c74810a663a5b8
Bruno Leonardo <135679874+brunols7@users.noreply.github.com>
2024-01-21 18:27:44 -0300

    Carro

---
+++ car.dart
@@ -0,0 +1,34 @@
+void main(){
+  
+  double taxa = 0.04;
+  
+  Carro carroDoBruno = Carro('Civic Touring', 'Preto', 'Honda', 2020, 145900);
+  Carro carroDoZe = Carro('Lancer', 'Prata', 'Mitsubishi', 2017, 61990);
+
+  print(carroDoBruno.toString());
+  print('Valor do IPVA: ${carroDoBruno.valorIPVA(taxa)}');
+
+  print(carroDoZe.toString());
+  print('Valor do IPVA: ${carroDoZe.valorIPVA(taxa)}');
+  
+}
+
+class Carro {
+
+  final String modelo;
+  final String cor;
+  final String marca;
+  final int ano;
+  final double preco;
+
+  Carro(this.modelo, this.cor, this.marca, this.ano, this.preco);
+
+  String toString(){
+    return 'Modelo: $modelo, Marca: $marca, Cor: $cor, Ano: $ano, Preço: $preco';
+  }
+
+  double valorIPVA(double taxa){
+    return taxa * preco;
+  }
+  
+}
